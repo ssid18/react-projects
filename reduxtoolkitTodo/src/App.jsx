d418c49ae300ee1efc75bdbe4395fdc504357dbb
ssid18 <siddhseth18@gmail.com>
2025-08-25 07:26:03 +0530

    Todo-List project  using Redux Toolkit

---
+++ reduxtoolkitTodo/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-white bg-gray-700'>
+      learning redux toolkit
+    </h1>
+    <AddTodo />
+    <Todos />
+    </>
+  )
+}
+
+export default App
